c0fbf8535d878f8722a5a7f39d117755e265c289
Umesh Bhatt <umesh.bhatt2002@gmail.com>
2025-10-11 17:21:06 +0530

    Added tests to validate hasVertex functionality for CinderGraph

---
+++ tests/integration/CinderGraph/functional/hasVertex_test.cpp
@@ -0,0 +1,57 @@
+#include "CinderPeak.hpp"
+#include "gtest/gtest.h"
+
+using namespace CinderPeak;
+
+class ListVertex : public CinderVertex {
+public:
+  int data;
+  ListVertex(int d = 0) : data(d) {}
+  bool operator==(const ListVertex &other) const { return data == other.data; }
+};
+
+class CinderGraphTest : public ::testing::Test {
+protected:
+  GraphCreationOptions directedOpts;
+  GraphCreationOptions undirectedOpts;
+
+  CinderGraphTest()
+      : directedOpts({GraphCreationOptions::Directed}),
+        undirectedOpts({GraphCreationOptions::Undirected}) {}
+};
+
+TEST_F(CinderGraphTest, HasVertexPrimitive) {
+  CinderGraph<int, int> intGraph;
+
+  EXPECT_TRUE(intGraph.addVertex(1).second);
+  EXPECT_TRUE(intGraph.addVertex(2).second);
+
+  EXPECT_TRUE(intGraph.hasVertex(1));
+  EXPECT_TRUE(intGraph.hasVertex(2));
+  EXPECT_FALSE(intGraph.hasVertex(3)); // not added
+}
+
+TEST_F(CinderGraphTest, HasVertexString) {
+  CinderGraph<std::string, float> stringGraph;
+
+  EXPECT_TRUE(stringGraph.addVertex("A").second);
+  EXPECT_TRUE(stringGraph.addVertex("B").second);
+
+  EXPECT_TRUE(stringGraph.hasVertex("A"));
+  EXPECT_TRUE(stringGraph.hasVertex("B"));
+  EXPECT_FALSE(stringGraph.hasVertex("C")); // not added
+}
+
+TEST_F(CinderGraphTest, HasVertexCustomType) {
+  CinderGraph<ListVertex, float> customGraph;
+
+  ListVertex v1(10);
+  ListVertex v2(20);
+
+  EXPECT_TRUE(customGraph.addVertex(v1).second);
+  EXPECT_TRUE(customGraph.addVertex(v2).second);
+
+  EXPECT_TRUE(customGraph.hasVertex(v1));
+  EXPECT_TRUE(customGraph.hasVertex(v2));
+  EXPECT_FALSE(customGraph.hasVertex(ListVertex(30))); // not added
+}
