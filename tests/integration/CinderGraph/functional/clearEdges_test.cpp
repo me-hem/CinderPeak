66e649cd4898e211951ca8984a9f9ff0c9c6b262
Umesh Bhatt <umesh.bhatt2002@gmail.com>
2025-10-11 16:50:52 +0530

    Added tests to validate clearEdges functionality for CinderGraph

---
+++ tests/integration/CinderGraph/functional/clearEdges_test.cpp
@@ -0,0 +1,91 @@
+#include "CinderPeak.hpp"
+#include "gtest/gtest.h"
+
+using namespace CinderPeak;
+
+class ListVertex : public CinderVertex {
+public:
+  int data;
+  ListVertex(int d = 0) : data(d) {}
+  bool operator==(const ListVertex &other) const { return data == other.data; }
+};
+
+class ListEdge : public CinderEdge {
+public:
+  float edge_weight;
+  ListEdge(float w = 0.0f) : edge_weight(w) {}
+  bool operator==(const ListEdge &other) const {
+    return edge_weight == other.edge_weight;
+  }
+};
+
+class CinderGraphTest : public ::testing::Test {
+protected:
+  GraphCreationOptions directedOpts;
+  GraphCreationOptions undirectedOpts;
+
+  CinderGraphTest()
+      : directedOpts({GraphCreationOptions::Directed}),
+        undirectedOpts({GraphCreationOptions::Undirected}) {}
+};
+
+TEST_F(CinderGraphTest, ClearWeightedEdges) {
+  CinderGraph<int, int> graph(directedOpts);
+
+  graph.addVertex(1);
+  graph.addVertex(2);
+  graph.addVertex(3);
+
+  EXPECT_TRUE(graph.addEdge(1, 3, 5).second);
+  EXPECT_TRUE(graph.addEdge(2, 3, 15).second);
+
+  EXPECT_EQ(graph.numVertices(), 3);
+  EXPECT_EQ(graph.numEdges(), 2);
+
+  graph.clearEdges();
+  EXPECT_EQ(graph.numEdges(), 0);
+  EXPECT_EQ(graph.numVertices(), 3); // vertices remain after clear
+}
+
+TEST_F(CinderGraphTest, ClearUnweightedEdges) {
+  CinderGraph<int, Unweighted> graph(directedOpts);
+
+  graph.addVertex(1);
+  graph.addVertex(2);
+  graph.addVertex(3);
+
+  EXPECT_TRUE(graph.addEdge(1, 2).second);
+  EXPECT_TRUE(graph.addEdge(2, 3).second);
+
+  EXPECT_EQ(graph.numVertices(), 3);
+  EXPECT_EQ(graph.numEdges(), 2);
+
+  graph.clearEdges();
+  EXPECT_EQ(graph.numEdges(), 0);
+  EXPECT_EQ(graph.numVertices(), 3); // vertices remain after clear
+}
+
+TEST_F(CinderGraphTest, ClearEdgesCustomTypes) {
+  CinderGraph<ListVertex, ListEdge> graph(directedOpts);
+
+  ListVertex v1(10);
+  ListVertex v2(20);
+  ListVertex v3(30);
+
+  graph.addVertex(v1);
+  graph.addVertex(v2);
+  graph.addVertex(v3);
+
+  ListEdge e1(1.5f);
+  ListEdge e2(2.5f);
+
+  EXPECT_TRUE(graph.addEdge(v1, v2, e1).second);
+  EXPECT_TRUE(graph.addEdge(v2, v3, e2).second);
+
+  EXPECT_EQ(graph.numVertices(), 3);
+  EXPECT_EQ(graph.numEdges(), 2);
+
+  graph.clearEdges();
+  EXPECT_EQ(graph.numEdges(), 0);
+  EXPECT_EQ(graph.numVertices(), 3); // vertices remain after clear
+}
