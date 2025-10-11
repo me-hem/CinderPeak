0d837bf2b369fa336630dbe85a56f1e1252c7958
Umesh Bhatt <umesh.bhatt2002@gmail.com>
2025-10-11 16:45:03 +0530

    Adde addVertex tests for CinderGraph

---
+++ tests/integration/CinderGraph/functional/addVertex_test.cpp
@@ -0,0 +1,46 @@
+#include "CinderPeak.hpp"
+#include "gtest/gtest.h"
+
+using namespace CinderPeak;
+
+class ListVertex : public CinderVertex {
+public:
+  int data;
+  ListVertex(int d = 0) : data(d) {}
+  bool operator==(const ListVertex &other) const { return data == other.data; }
+};
+
+class CinderGraphTest : public ::testing::Test {
+protected:
+  GraphCreationOptions directedOpts;
+  GraphCreationOptions undirectedOpts;
+
+  CinderGraphTest()
+      : directedOpts({GraphCreationOptions::Directed}),
+        undirectedOpts({GraphCreationOptions::Undirected}) {}
+};
+
+TEST_F(CinderGraphTest, AddVertexPrimitive) {
+  CinderGraph<int, int> intGraph;
+
+  EXPECT_TRUE(intGraph.addVertex(1).second);
+  EXPECT_FALSE(intGraph.addVertex(1).second); // duplicate
+}
+
+TEST_F(CinderGraphTest, AddVertexString) {
+  CinderGraph<std::string, float> stringGraph;
+
+  EXPECT_TRUE(stringGraph.addVertex("A").second);
+  EXPECT_FALSE(stringGraph.addVertex("A").second); // duplicate
+}
+
+TEST_F(CinderGraphTest, AddVertexCustomType) {
+  CinderGraph<ListVertex, float> customGraph;
+
+  ListVertex v1(10);
+  ListVertex v2(20);
+
+  EXPECT_TRUE(customGraph.addVertex(v1).second);
+  EXPECT_TRUE(customGraph.addVertex(v2).second);
+  EXPECT_FALSE(customGraph.addVertex(v1).second); // duplicate
+}
